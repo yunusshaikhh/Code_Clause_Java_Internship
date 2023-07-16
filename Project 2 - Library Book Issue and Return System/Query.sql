f9281618028154cf5b53d803fdab46c00ec8269e
Yunus Shaikh <120585277+yunusshaikhh@users.noreply.github.com>
2023-07-16 15:47:16 +0530

    First Commit

--- Project 2 - Library Book Issue and Return System/Query.sql
+++ Project 2 - Library Book Issue and Return System/Query.sql
@@ -3,13 +3,13 @@ drop database library;
 
 select * from books;
 insert into books value
-(111,"Fundamentals of C","Rahul",1),(112,"Data Smart","Foreman",1),
-(113,"God Created the Integers","Hawking",1),(114,"Superfreakonomics","Dubner",1),
-(115,"Orientalism","Edward",1),(116,"Jurassic Park","Michael",1),
-(117,"Freakonomics","Dubner",1),(118,"City of Joy","Dominique",1),
-(119,"Free Will","Harris",1),(120,"Electric Universe","Bodanis",1);
+(111,"Fundamentals of C","Rahul",1),(112,"Computer Fundamentals","Yunus",1),
+(113,"Machine Learning","Donald",1),(114,"Artificial Intelligence","Harris",1),
+(115,"Cyber Security Advanced","Jacob",1),(116,"Cloud Computing - AWS","Juliet",1),
+(117,"Robotics - MIT","John",1),(118,"Introduction to Computer Science","Michale",1),
+(119,"Android Programming","Obama",1),(120,"Database Management Systems","Mathew",1);
 
 select * from students;
 insert into students value
-(6110,"Chhagan",1),(6111,"Dev",2),(6120,"Jeet",4),
-(6123,"Hammaad",0),(6124,"Juned",1),(6139,"priyanshu",0);
+(6110,"Yunus",1),(6111,"Mohan",2),(6120,"Preeti",4),
+(6123,"Camraly",0),(6124,"Alyeena",1),(6139,"Johnathon",0);
